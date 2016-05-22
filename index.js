7692f58adb5ee1a01b55b8e802844f7983e4c212
rafrex <code@rafrex.com>
2016-05-22 09:34:11 -0700

    Make touch detection code cleaner

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 'use strict';
 
 function detectTouch() {
-  if (
+  return !!(
     'ontouchstart' in window ||
     window.navigator.maxTouchPoints > 0 ||
     window.navigator.msMaxTouchPoints > 0 ||
     window.DocumentTouch && document instanceof DocumentTouch
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+  );
 }
 
 exports.detectTouch = detectTouch;
